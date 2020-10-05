af623a9677564da20b0d243a732c63843e2a9627
Ashley Hunt <ashhunt07@gmail.com>
2020-10-05 00:07:18 -0500

    testing express.static

--- server.js
+++ server.js
@@ -28,17 +28,98 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-// Displays index when the application is loaded
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// // Displays index when the application is loaded
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, "./public/index.html"));
+// });
+
+// // Displays notes
+// app.get("/notes", (req, res) => {
+//     res.sendFile(path.join(__dirname, "./public/notes.html"));
+// });
+
+
+// Displays a single character, or returns false
+app.get("/api/notes", (req, res) => {
+
+    readFileAsync("./db/db.json", "utf8")
+        .then((result, err) => {
+            if (err) console.log(err);
+            return res.json(JSON.parse(result));
+        });
 });
 
-// Displays notes
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 
+//Saves a note
+app.post("/api/notes", (req, res) => {
+    let newNote = req.body;
+
+    readFileAsync("./db/db.json", "utf8")
+        .then((result, err) => {
+            if (err) console.log(err);
+            return Promise.resolve(JSON.parse(result));
+        })
+        .then(data => {
+
+            newNote.id = getLastIndex(data) + 1;
+
+            (data.length > 0) ? data.push(newNote): data = [newNote];
+            return Promise.resolve(data);
+        })
+        .then(data => {
+            //write the new file
+            writeFileAsync("./db/db.json", JSON.stringify(data));
+            res.json(newNote);
+        })
+        .catch(err => {
+            if (err) throw err;
+        });
+});
+
+
+//Deletes saved note
+app.delete('/api/notes/:id', (req, res) => {
+
+    let id = req.params.id;
+
+    readFileAsync("./db/db.json", "utf8")
+        .then((result, err) => {
+            if (err) console.log(err);
+            return Promise.resolve(JSON.parse(result));
+        })
+        .then(data => {
+
+            data.splice(data.indexOf(data.find(element => element.id == id)), 1);
+            return Promise.resolve(data);
+        })
+        .then(data => {
+
+            writeFileAsync("./db/db.json", JSON.stringify(data));
+            res.send("OK");
+        })
+        .catch(err => {
+            if (err) throw err;
+        });
+});
+
+
+// Console log if user gets a 404 message
+app.use(function(req, res, next) {
+    res.status(404).send("Sorry, we couldn't find that!")
+})
+
+
+function getLastIndex(data) {
+    if (data.length > 0) return data[data.length - 1].id;
+    return 0;
+}
 
 // Starts the server to begin listening
 app.listen(PORT, function() {

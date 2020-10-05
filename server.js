0b4950abacbef75d70058e20208d5319f3809ad4
Ashley Hunt <ashhunt07@gmail.com>
2020-10-05 00:03:49 -0500

    adding links to html files and running to test

--- server.js
+++ server.js
@@ -15,6 +15,31 @@ const app = express();
 const PORT = process.env.PORT || 9000;
 
 
+// Quick reference to Async files (save on some lines of code)
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Folder the browser can see
+app.use(express.static("public"));
+
+
+// Displays index when the application is loaded
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+// Displays notes
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+
+
 // Starts the server to begin listening
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
